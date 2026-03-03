ff705b4228ffd474d7636881d5ae234beace2749
Sahil Ahuja <sahilahuja@gmail.com>
2026-03-03 13:10:40 +0530

    Adding gaap command

--- stow/macos/zsh/.zshrc_aliases.sh
+++ stow/macos/zsh/.zshrc_aliases.sh
@@ -41,6 +41,7 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gst='git status'
 alias gcam='git commit -am'
+gaap() { git add --all && git commit -m "${1:?Usage: gaap \"commit message\"}" && git push }
 
 alias gk='gitk --all&'
 alias gx='gitx --all'
