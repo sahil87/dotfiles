91eba42143108dfc515d79f20cf7ad0c0e8283a4
Sahil Ahuja <sahilahuja@gmail.com>
2026-03-10 15:09:00 +0530

    Add ~/.cargo/bin to PATH for both macOS and Linux

--- stow/linux/zsh/.zshrc_os.sh
+++ stow/linux/zsh/.zshrc_os.sh
@@ -65,5 +65,5 @@ case ":$PATH:" in
 esac
 
 # Path (first match wins) - OS_PATH_EXTRAS set by OS-specific config
-path=($LIFETRACKER_DIR/bin $CODE_DIR/bin $OS_PATH_EXTRAS $path .)
+path=($HOME/.cargo/bin $LIFETRACKER_DIR/bin $CODE_DIR/bin $OS_PATH_EXTRAS $path .)
 
